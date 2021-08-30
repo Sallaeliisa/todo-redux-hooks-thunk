445f8dc0eef590ba861913ae09ad2edd3ce817b1
Vuorikko Salla <salla.vuorikko@gmail.com>
2021-08-30 17:38:36 +0300

    basic styling

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { createNote} from '../store/actions/actions';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { createNote } from "../store/actions/actions";
 
 const NewNote = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const addToDo = async (e) => {
-        e.preventDefault();
-        const text = e.target.noteInput.value;
-        dispatch(createNote(text));
-        e.target.noteInput.value = '';
-    };
+  const addToDo = async (e) => {
+    e.preventDefault();
+    const text = e.target.noteInput.value;
+    dispatch(createNote(text));
+    e.target.noteInput.value = "";
+  };
 
-    return (
-        <form onSubmit={addToDo}>
-            <input type='text' name='noteInput' />
-            <input type='submit' value='Add note' />
-        </form>
-    );
+  return (
+    <div>
+      <header><h1>To Do</h1></header>
+      <form onSubmit={addToDo}>
+        <input type="text" name="noteInput" />
+        <input type="submit" value="Add note" className="submitBtn"/>
+      </form>
+    </div>
+  );
 };
 
 export default NewNote;
