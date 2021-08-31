b55d74e312227ebcb4e8ce78a0553601fc7720a8
Vuorikko Salla <salla.vuorikko@gmail.com>
2021-08-31 16:01:09 +0300

    app displays also the number of notes

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { createNote } from "../store/actions/actions";
 
 const NewNote = () => {
   const dispatch = useDispatch();
+  const notes = useSelector((state) => state);
 
   const addToDo = async (e) => {
     e.preventDefault();
@@ -16,6 +17,7 @@ const NewNote = () => {
     <div>
       <header>
         <h1>To Do</h1>
+        <p>Number of notes: {notes.length}</p>
       </header>
       <form onSubmit={addToDo}>
         <input type="text" name="noteInput" />
